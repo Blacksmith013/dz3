87720d8faed5826130edf35ecf9792fed8b0e190
Vladimir <kuznetsov.vi03@mail.ru>
2022-08-19 18:36:13 +0300

    table of cubes

---
+++ Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+if (number < 0)
+    Console.WriteLine("Введите положительное число");
+
+while (number >= count)
+{
+    double result = Math.Pow(count, 3);
+    count++;
+    Console.Write(result + " ");
+}
